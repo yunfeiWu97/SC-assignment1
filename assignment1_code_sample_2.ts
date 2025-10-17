74ab54689e406a2f60a9a4fcb79f094e9371b290
Dylan <dkaspick2@rrc.ca>
2025-10-17 16:13:35 -0500

    Update assignment1_code_sample_2.ts vulnerabilities
    Addressed the vulnerabilities pointed out in the PR.

--- assignment1_code_sample_2.ts
+++ assignment1_code_sample_2.ts
@@ -9,6 +9,22 @@ const dbConfig = {
     password: 'secret123',
     database: 'mydb'
 };
+/*
+An easy fix for this issue would be to use an environment variable for
+data that you don't want pushed with your code.
+
+First we have to create a .env file to hold our variables and install the
+dotenv node package to easily read from this file.
+
+The .env file will hold DB_PASSWORD='secret123'
+
+In typescript this might look like:
+const dotenv = require('dotenv');
+dotenv.config();
+password: 'process.env.DB_PASSWORD'
+
+Information source: https://medium.com/the-node-js-collection/making-your-node-js-work-everywhere-with-environment-variables-2da8cdf6e786
+*/
 
 function getUserInput(): Promise<string> {
     const rl = readline.createInterface({
@@ -23,6 +39,10 @@ function getUserInput(): Promise<string> {
         });
     });
 }
+/*
+To sanitize this input we would use something like a regex that checks for
+escape characters and sets a character limit for the input.
+*/
 
 function sendEmail(to: string, subject: string, body: string) {
     exec(`echo ${body} | mail -s "${subject}" ${to}`, (error, stdout, stderr) => {
@@ -31,6 +51,13 @@ function sendEmail(to: string, subject: string, body: string) {
         }
     });
 }
+/*
+To mitigate this injection issue we can start by using a library that can
+handle the validation of an email address for us, making sure it follows email
+syntax. For the other inputs we could again use regex to check the strings for
+and escape characters that will allow for command injection. Or if there is an
+existing library for sending emails we could also use that to handle everything.
+*/
 
 function getData(): Promise<string> {
     return new Promise((resolve, reject) => {
@@ -41,6 +68,10 @@ function getData(): Promise<string> {
         }).on('error', reject);
     });
 }
+/*
+Here we would just need to replace using the http Node module with the https one.
+https://nodejs.org/api/https.html#httpsgetoptions-callback
+*/
 
 function saveToDb(data: string) {
     const connection = mysql.createConnection(dbConfig);
@@ -56,10 +87,17 @@ function saveToDb(data: string) {
         connection.end();
     });
 }
+/*
+This one I believe is a simple fix. All we need to do is seperate the data
+out of the original string for the query.
+const query = `INSERT INTO mytable (column1, column2) VALUES ('?', 'Another Value')`;
+connection.query(query, data,(error, results)
+*/
 
 (async () => {
     const userInput = await getUserInput();
     const data = await getData();
     saveToDb(data);
     sendEmail('admin@example.com', 'User Input', userInput);
+
 })();
